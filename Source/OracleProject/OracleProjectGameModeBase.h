9d9764b7767d44906015f36917983a6c687b1aaa
Mootjuh <haddouti_m@live.nl>
2017-12-22 10:12:35 +0100

    initial repo push

---
+++ Source/OracleProject/OracleProjectGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OracleProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ORACLEPROJECT_API AOracleProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
